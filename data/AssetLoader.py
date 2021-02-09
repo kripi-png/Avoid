a693a1917149b5655ae18a43aee6695e3b4ce447
kripi-png <mainkriiper@gmail.com>
2021-02-10 01:18:05 +0200

    new menu click sound

--- data/AssetLoader.py
+++ data/AssetLoader.py
@@ -3,7 +3,6 @@ from .Utils import *
 class AssetLoader(object):
     def __init__(this):
         super(AssetLoader, this).__init__()
-
         print("Loading Images")
         this.imageEnemyEye = loadImage("eyeBoss")
         this.imagePlayer = loadImage("player")
